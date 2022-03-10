7aa6be28b7c6d7979cf432c1a0fc8f4efdeb788b
magalhaes13 <arthurmagalhaes13@hotmail.com>
2022-03-10 17:04:02 -0300

    api cep

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,6 +1,12 @@
-// criar as coisas depois
 
-function sair(){
-    localStorage.removeItem('token')
-    window.location.href = '/login'
-}
+
+
+// document.getElementById('cep').addEventListener('onkeyup', async ()=>{
+//     const {data} = await axios.get('https://viacep.com.br/ws/01001000/json/')
+//     console.log(data)
+// })
+
+// async function getCep(){
+//     const {data} = await axios.get('https://viacep.com.br/ws/01001000/json/')
+//     console.log(data)
+// }
