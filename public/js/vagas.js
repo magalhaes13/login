3849418df40cb518bfc97882712d5f02006501a8
magalhaes13 <arthurmagalhaes13@hotmail.com>
2022-03-17 09:27:19 -0300

    melhorias

--- public/js/vagas.js
+++ public/js/vagas.js
@@ -1,6 +1,25 @@
-// criar as coisas depois
+document.getElementById('botaoSalvar').addEventListener('click', async (e) => {
+    e.preventDefault();
 
-function sair(){
-    localStorage.removeItem('token')
-    window.location.href = '/login'
-}
+    try{
+
+        const NOME_VAGA = document.getElementById('nome_vaga').value
+        const SALARIO_VAGA = document.getElementById('salario_vaga').value
+        const NOME_EMPRESA = document.getElementById('nome_empresa').value
+        const CIDADE_VAGA = document.getElementById('cidade_vaga').value
+        const DESCRICAO_VAGA = document.getElementById('descricao_vaga').value
+
+
+        const data = await axios.get(`/vagas/${NOME_VAGA}/${NOME_VAGA}/${NOME_EMPRESA}/${CIDADE_VAGA}/${DESCRICAO_VAGA}`)
+        console.log(data)
+        
+        console.log("aaaaaqqqqq")
+        window.location.href = '/vagas'
+    }
+
+    catch(error){
+        console.log(error.response)
+        if(error.response.status === 401)
+        alert(error.response.data)
+    }
+})
--- routes/vagas.js
+++ routes/vagas.js
@@ -1,9 +1,23 @@
 const { Router } = require('express')
 const router = Router();
+const mysql = require("../config/bd")
 // import axios from "axios"
 
 router.get('/', async(req,res)=>{
     res.render('vagas')
 })
+
+router.get('/vagas', async (req, res) => {          //get: buscar  ////  
+                                                                    //async: significa que o valor de retorno da função será, "por baixo dos panos", uma Promise
+    try {                                                           //await: esperar
+        const { usuario, senha } = res.params
+        const data = await mysql.raw(`SELECT * FROM VAGAS WHERE ID_VAGAS = '${ID_VAGAS}'`)
+    }
+    catch(error) {
+        console.log(error)
+        return res.status(500).send("ERROR")     
+
+    }
+});
 
 module.exports = router;
