e8885e50c3273780e0e7ab680d426d813765b61f
magalhaes13 <arthurmagalhaes13@hotmail.com>
2022-05-04 01:03:12 -0300

    update: campos perfil, salvando e pegando os valores

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,43 +1,89 @@
 mostrarPerfil();
 
-async function mostrarPerfil(){
+async function mostrarPerfil() {
+  try {
+    const data = await axios.get("/perfil");
+    console.log(data.data[0]);
 
-
-    try{
-        const data = await axios.get("/perfil/meuPerfil")
-        console.log(data.data[0]);
-
-        $('#dadosPerfil').empty()
-        for(const item of data.data[0]){
-            $('#dadosPerfil').append(
-                `<input class="dadosPerfil" type="text" size="50" placeholder="Nome Completo">`
-            )
-        }
-
+    $("#dadosPerfil").empty();
+    for (const item of data.data[0]) {
+      $("#dadosPerfil").append(
+        `<input class="dadosPerfil" type="text" size="50" placeholder="Nome Completo">`
+      );
     }
-    catch(ERROR){
-        console.log(ERROR);
+  } catch (ERROR) {
+    console.log(ERROR);
+  }
+
+  try {
+    const idPerfil = document.getElementById("idPerfil").value;
+
+    if (idPerfil !== "") {
+      const inputs = document.getElementsByClassName("dadosPerfil");
+
+      for (var i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = true;
+      }
     }
+  } catch (ERROR) {
+    console.log(ERROR);
+  }
+}
 
-    
+async function savePerfil() {
+  try {
+    let dataPerfil = {
+      IDPERFIL: "",
+      NOME: "",
+      CPF: "",
+      EMAIL: "",
+      RG: "",
+      SEXO: "OUTROS",
+      DATANASCIMENTO: "",
+      CEP: "",
+      LOGRADOURO: "",
+      NUMERO: "",
+      BAIRRO: "",
+      ESTADO: "",
+      COMPLEMENTO: "",
 
-    try{
-        const idPerfil = document.getElementById("idPerfil").value;
+   };
 
-        if(idPerfil !== ""){
-            
-            const inputs = document.getElementsByClassName("dadosPerfil");
+    dataPerfil.NOME = document.getElementById("nomeCompleto").value;
+    dataPerfil.CPF = document.getElementById("cpf").value;
+    dataPerfil.EMAIL = document.getElementById("email").value;
+    dataPerfil.RG = document.getElementById("rg").value;
+    dataPerfil.SEXO = getValueRadio();
+    dataPerfil.DATANASCIMENTO = document.getElementById("data").value;
+    dataPerfil.CEP = document.getElementById("cep").value;
+    dataPerfil.LOGRADOURO = document.getElementById("logradouro").value;
+    dataPerfil.NUMERO = document.getElementById("numero").value;
+    dataPerfil.BAIRRO = document.getElementById("bairro").value;
+    dataPerfil.ESTADO = document.getElementById("uf").value;
+    dataPerfil.COMPLEMENTO = document.getElementById("complemento").value;
 
-            for(var i = 0; i<inputs.length; i++){
-                inputs[i].disabled = true;
-            }
-        }
-    }
-    catch(ERROR){
-        console.log(ERROR);
-    }
+
+    console.log(dataPerfil);
+
+    const {data} = await axios.post("/perfil/meuPerfil",dataPerfil)
+    console.log(data)
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function getValueRadio() {
+  if (document.getElementById("op1").checked === true) {
+    return document.getElementById("op1").value;
+  } else if (document.getElementById("op2").checked === true) {
+    return document.getElementById("op2").value;
+  } else {
+    return document.getElementById("op3").value;
+  }
 }
 
 
-        // const data = await axios.get("/perfil/meuPerfil")
-        // console.log(data.data[0]);
+
+// const data = await axios.get("/perfil/meuPerfil")
+// console.log(data.data[0]);
--- routes/perfil.js
+++ routes/perfil.js
@@ -8,11 +8,18 @@ router.get('/', async(req,res)=>{
     res.render('perfil')
 })
 
-router.get('/meuPerfil', async (req, res) => {          //get: buscar  ////  
+router.post('/meuPerfil', async (req, res) => {          //get: buscar  ////  
                                                                     //async: significa que o valor de retorno da função será, "por baixo dos panos", uma Promise
     try {                                                           //await: esperar
-        const data = await mysql.raw(`SELECT * FROM USUARIO WHERE ID_USUARIO = ${ID_USUARIO}`)
-        return res.status(200).json(data)
+        // const data = await mysql.raw(`SELECT * FROM USUARIO WHERE ID_USUARIO = ${ID_USUARIO}`)
+        // return res.status(200).json(data)
+
+        const {IDPERFIL,NOME,CPF,EMAIL,RG,SEXO,DATANASCIMENTO,CEP,LOGRADOURO,NUMERO,BAIRRO,ESTADO,COMPLEMENTO} = await req.body;
+        const idUsuario = await mysql.raw(`INSERT INTO PERFIL(NOME,CPF,EMAIL,RG,SEXO,DATANASCIMENTO,CEP,LOGRADOURO,NUMERO,BAIRRO,ESTADO,COMPLEMENTO) VALUES('${NOME}', '${CPF}','${EMAIL}','${RG}','${SEXO}','${DATANASCIMENTO}','${CEP}','${LOGRADOURO}','${NUMERO}','${BAIRRO}','${ESTADO}','${COMPLEMENTO}') `)
+                console.log(idUsuario[0].insertId)
+                return res.status(200).json({id: idUsuario[0].insertId})
+
+
     }
     catch(error) {
         console.log(error)
